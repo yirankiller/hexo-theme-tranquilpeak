9b6c4bd50e86ff5dd07a9d90145f2007d46cff74
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-07-18 13:07:32 +0200

    v1.2.0

--- assets/js/tranquilpeak.js
+++ assets/js/tranquilpeak.js
@@ -8,7 +8,7 @@
      * @constructor
      */
     var AboutCard = function() {
-        this.$openBtn   = $("#sidebar, #header").find("a[href$='/#about']");
+        this.$openBtn   = $("#sidebar, #header").find("a[href*='#about']");
         this.$closeBtn  = $('#about-btn-close');
         this.$blog      = $('#blog');
         this.$about     = $('#about');
@@ -22,18 +22,16 @@
          */
         run: function() {
             var self = this;
-
             // Detect click on open button
             self.$openBtn.click(function(e) {
                 e.preventDefault();
                 self.play();
-            })
-
+            });
             // Detect click on close button
             self.$closeBtn.click(function(e) {
                 e.preventDefault();
                 self.playBack();
-            })
+            });
         },
 
         /**
@@ -664,6 +662,88 @@
             var postBottomBar = new PostBottomBar();
             postBottomBar.run();
         }
+    });
+}(jQuery);;+function($) {
+    'use strict';
+
+    // Open and close the share options bar
+
+    /**
+     * ShareOptionsBar
+     * @constructor
+     */
+    var ShareOptionsBar = function() {
+        this.$shareOptionsBar = $('#share-options-bar');
+        this.$openBtn      = $('.btn-open-shareoptions');
+        this.$closeBtn     = $('#share-options-mask');
+    };
+
+    ShareOptionsBar.prototype = {
+
+        /**
+         * Run ShareOptionsBar feature
+         */
+        run: function() {
+            var self = this;
+
+            // Detect the click on the open button
+            self.$openBtn.click(function() {
+                if (!self.$shareOptionsBar.hasClass('opened')) {
+                    self.openShareOptions();
+                    self.$closeBtn.show();
+
+                }
+            });
+
+            // Detect the click on the close button
+            self.$closeBtn.click(function() {
+                if (self.$shareOptionsBar.hasClass('opened')) {
+                    self.closeShareOptions();
+                    self.$closeBtn.hide();
+                }
+            });
+        },
+
+        /**
+         * Open share options bar
+         */
+        openShareOptions: function() {
+            var self = this;
+
+            // Check if the share option bar isn't opened and prevent multiple click on the open button with `.processing` class
+            if (!self.$shareOptionsBar.hasClass('opened') && !this.$shareOptionsBar.hasClass('processing')) {
+                // Open the share option bar
+                self.$shareOptionsBar.addClass('processing opened');
+
+                setTimeout(function() {
+                    self.$shareOptionsBar.removeClass('processing');
+                }, 250);
+            }
+        },
+
+        /**
+         * Close share options bar
+         */
+        closeShareOptions: function() {
+            var self = this;
+
+            // Check if the share options bar is opened and prevent multiple click on the close button with `.processing` class
+            if (self.$shareOptionsBar.hasClass('opened') && !this.$shareOptionsBar.hasClass('processing')) {
+                // Close the share option bar
+                self.$shareOptionsBar
+                    .addClass('processing')
+                    .removeClass('opened');
+
+                setTimeout(function() {
+                    self.$shareOptionsBar.removeClass('processing');
+                }, 250);
+            }
+        }
+    };
+
+    $(document).ready(function() {
+        var shareOptionsBar = new ShareOptionsBar();
+        shareOptionsBar.run();
     });
 }(jQuery);;+function($) {
     'use strict';
